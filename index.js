41c28e1f2cf179b33c09c24f27a32b2bf62dfac2
Raphael Viegas <45077597+raphaelviegas@users.noreply.github.com>
2023-07-20 12:17:58 -0300

    update address

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const expressNunjucks = require('express-nunjucks');
+const nunjucks = require('nunjucks');
 const app = express();
 const globalVariables = require('./functions/data/globalVariables.json')
 
 app.set('views', __dirname + '/functions/templates');
+app.use(express.json())
 
 const njk = expressNunjucks(app, {
   watch: true,
@@ -16,6 +18,19 @@ app.get('/:page', (req, res) => {
   var data = require(`./functions/data/${req.params.page}.json`)
   res.render(req.params.page, data);
 });
+
+
+const njkRaw = nunjucks.configure('./functions/templates', { autoescape: true })
+njkRaw.addGlobal('global', globalVariables.global);
+// Post request that returns the nunjucks generated html
+app.post('/:page', (req, res) => {
+  // var data = require(`./functions/data/${req.params.page}.json`)
+  const data = req.body
+  console.log(data)
+  const htmlContent = njkRaw.render(`${req.params.page}.html`, data)
+  res.set('Content-Type', 'text/html')
+  res.send(Buffer.from(htmlContent))
+})
 
 app.listen(3000);
 console.log('App running on port 3000')
